4d785d061a812e11d2de4292196f4cfb5c0b0962
fliper6 <filipa.alvesantos@outlook.com>
2019-12-28 00:02:57 +0000

    cliente e tal

--- src/Cliente/Client.java
+++ src/Cliente/Client.java
@@ -9,41 +9,39 @@ public class Client {
     public static void main(String[] args) throws Exception{
         Socket socket = new Socket("127.0.0.1",12345);
         String input;
+        
         Scanner sc = new Scanner(System.in);
-
         BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
         PrintWriter out = new PrintWriter(socket.getOutputStream());
-
-        while(!(input=sc.nextLine()).equals("q")) {
-            out.println(input);
-            out.flush();
-            System.out.println("Server Sent: " + in.readLine());
-        }
-
-        socket.shutdownOutput();
-        socket.shutdownInput();
-        socket.close();
-        /*
+        
         try{
-
             Socket s = new Socket("localhost",12345);
 
             BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(s.getOutputStream()));
             BufferedReader br = new BufferedReader(new InputStreamReader(s.getInputStream()));
             Menu menu = new Menu();
-            Stub stub = new Stub(menu,bw);
-            //ClientReader cr = new ClientReader(br, menu);
+            Writer writer = new Writer(menu,bw);
+            Reader reader = new Reader(menu,br);
 
-            //Thread reader = new Thread(cr);
-            Thread stubs = new Thread(stub);
+            Thread t_reader = new Thread(reader);
+            Thread t_writer = new Thread(writer);
 
-            //reader.start();
-            stubs.start();
+            t_reader.start();
+            t_writer.start();
         }
         catch(Exception e){
             System.out.println(e.getMessage());
         }
-        */
 
+        // Para podermos dar quit do programa a qualquer momento
+        while(!(input=sc.nextLine()).equals("q")) {
+            out.println(input);
+            out.flush();
+            System.out.println("Server Sent: " + in.readLine());
+        }
+        
+        socket.shutdownOutput();
+        socket.shutdownInput();
+        socket.close();
     }
 }
--- src/Cliente/Writer.java
+++ src/Cliente/Writer.java
@@ -1,17 +1,21 @@
 package Cliente;
 
+// Imports
 import java.io.BufferedWriter;
+import java.io.IOException;
 
-public class Stub implements Runnable {
+public class Writer implements Runnable {
+
+    // Variáveis de Instância
     private Menu menu;
     private BufferedWriter output;
 
-    public Stub(Menu m, BufferedWriter o) {
+    public Writer(Menu m, BufferedWriter o) {
         output = o;
         menu = m;
     }
 
-    @Override
+    // Está constantemente a ler a "choice" do user  da classe "Menu" e ...
     public void run() {
         int choice;
         menu.show();
@@ -24,29 +28,20 @@ public class Stub implements Runnable {
         }
     }
 
+    //... dependendo da escolha, executa a ação correspondente
     private void parse (Integer choice) throws Exception {
         switch (menu.getState()) {
-            case UNAUTH:
-                if (choice==1) {
-                    //login();
-                }
-                if (choice==2) {
-                    //signup();
-                }
-                if (choice==0) {
+            case NOTLOGGED:
+                if (choice == 0) 
                     System.exit(0);
-                }
+                if (choice == 1) 
+                    System.out.println("login");
+                    login_signup(1);
+                if (choice == 2) 
+                    System.out.println("signup");
+                    login_signup(2);
                 break;
-            case AUTH:
-                if (choice==1) {
-                    //sendMessage("QUEUE");
-                }
-                if (choice==2) {
-                    //sendMessage("INFO");
-                }
-                if (choice==0) {
-                    //sendMessage("LOGOUT");
-                }
+            case LOGGED:
                 break;
             case REGISTERING:
                 break;
@@ -59,4 +54,19 @@ public class Stub implements Runnable {
         }
     }
 
+    // No caso do login/signup, recebemos 
+    private void login_signup(int i) throws IOException {
+        String username = menu.lerDadosUser("Username: ");
+        String password = menu.lerDadosUser("Password: ");
+        String query;
+
+        if (i == 1)
+            query = "LOGIN"+"-"+username+","+password;
+        else 
+            query = "REGISTER"+"-"+username+","+password;
+
+        output.write(query);
+        output.newLine();
+        output.flush();
+    }
 }
